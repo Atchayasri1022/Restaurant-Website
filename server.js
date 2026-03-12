544fd77e5f36e9d6e97ca57c44b72e7179ff1494
Atchayasri <atchayasribalasundaram@gmail.com>
2026-03-12 05:36:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res)=>{
+  res.send("Welcome to my website")
+})
+
+app.get("/api",(req,res)=>{
+  res.json({message:"Hello API"})
+})
+
+app.listen(3000,()=>{
+  console.log("Server running on port 3000")
+})
